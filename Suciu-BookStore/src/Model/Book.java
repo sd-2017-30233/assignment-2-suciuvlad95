32ad04eec94f0134d717c969d686ee515c15d1e6
Suciu Vlad-Vasile <Suciu Vlad-Vasile>
2017-04-26 15:08:26 +0300

    added model

---
+++ Suciu-BookStore/src/Model/Book.java
@@ -0,0 +1,61 @@
+package Model;
+
+public class Book {
+
+    private String id;
+    private String title;
+    private String author;
+    private String genre;
+    private String price;
+    private String quantity;
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+   
+}
